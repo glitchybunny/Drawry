e87043b254931b884f2d50228eb15cd91eaf6dba
Bunny <12066089+glitchybunny@users.noreply.github.com>
2021-08-09 14:43:58 +1000

    Simplified/refactored some global variables

--- static/js/client.js
+++ static/js/client.js
@@ -11,16 +11,15 @@ window.crypto.getRandomValues(array);
 const ID = Cookies.get('id') ? Cookies.get('id') : (array[0].valueOf() + 1).toString();
 const SESSION_KEY = array[1].valueOf().toString(16) + array[2].valueOf().toString(16); // currently unused
 const SOCKET = io.connect(document.documentURI);
+
+/// --- GAME CONSTANTS --- ///
 const USERS = {};
 const BOOKS = {};
+const ROOM = {};
+const ROUND = {};
 
 /// --- VARIABLES --- ///
 var name = undefined;
-var room = undefined;
-var roomSettings = undefined;
-var round = undefined;
-var host = undefined;
-var isHost = false;
 
 // Ensure browser compatibility
 if (!('getContext' in document.createElement('canvas'))) {
@@ -47,31 +46,30 @@ SOCKET.on('connect', () => {
 
 // Let client know they've joined the room successfully
 SOCKET.on('joined', (data) => {
-    console.log("Joined room '" + data.room + "'");
+    console.log("Joined room '" + data.roomCode + "'");
     hide(byId('loading'));
 
     // update local game values
-    room = data.room;
-    roomSettings = data.settings;
+    ROOM.code = data.roomCode;
+    ROOM.settings = data.settings;
+    ROOM.host = data.host;
     data.users.forEach((o) => {
         USERS[o.id] = {name: htmlDecode(o.name)}
     })
-    isHost = (data.host === ID);
-    host = getName(data.host);
 
     // switch to game screen
     hide(byId('mainJoin'));
     show(byId('mainGame'));
 
     // update DOM
-    byId('roomCode').textContent = room;
+    byId('roomCode').textContent = ROOM.code;
     updatePlayerList();
     updateHost();
     updateSettings();
 
     // set cookies so game remembers the session (in case user accidentally closes tab and needs to rejoin)
     Cookies.set('name', name);
-    Cookies.set('room', room);
+    Cookies.set('room', ROOM.code);
     Cookies.set('id', ID);
 })
 
@@ -93,15 +91,14 @@ SOCKET.on('userLeave', (userID) => {
 
 // Update which user is the host
 SOCKET.on('userHost', (userID) => {
-    isHost = (userID === ID);
-    host = getName(userID);
+    ROOM.host = userID;
     updateHost();
 });
 
 // Update settings
 SOCKET.on('settings', (data) => {
     // Update the settings on the client to reflect the new settings
-    roomSettings = data;
+    ROOM.settings = data;
     updateSettings();
 })
 
@@ -109,19 +106,6 @@ SOCKET.on('settings', (data) => {
 /// --- GAME --- ///
 // Start the game
 SOCKET.on('startGame', (data) => {
-    // Update DOM
-    hide(byId('setupSection'));
-    hide(byId('inviteSection'));
-    show(byId('gameSection'));
-    show(byId('bookSection'));
-
-    // Set round info
-    round = {
-        page: 0,
-        book: ID,
-        type: data.start
-    };
-
     // Load book page information
     for (let _id in data.books) {
         BOOKS[_id] = {
@@ -130,6 +114,17 @@ SOCKET.on('startGame', (data) => {
             book: data.books[_id]
         }
     }
+
+    // Set round info
+    ROUND.page = 0;
+    ROUND.book = BOOKS[ID];
+    ROUND.type = data.start;
+
+    // Update DOM
+    hide(byId('setupSection'));
+    hide(byId('inviteSection'));
+    show(byId('gameSection'));
+    show(byId('bookSection'));
     updateBookList();
 
     // Load start page
@@ -144,7 +139,7 @@ SOCKET.on('startGame', (data) => {
             resizeCanvas();
             break;
     }
-    byId('pageCurrent').textContent = (round.page + 1).toString();
+    byId('pageCurrent').textContent = (ROUND.page + 1).toString();
 });
 
 // Update book title
@@ -168,22 +163,22 @@ SOCKET.on('nextPage', () => {
     show(byId('gamePrevious'));
 
     // Increment page number
-    round.page += 1;
-    byId('pageCurrent').textContent = (round.page + 1).toString();
+    ROUND.page += 1;
+    byId('pageCurrent').textContent = (ROUND.page + 1).toString();
 
     // Figure out book and previous page
     for (let _id in BOOKS) {
-        if (BOOKS[_id].book[round.page] === ID) {
-            round.book = _id;
+        if (BOOKS[_id].book[ROUND.page] === ID) {
+            ROUND.book = BOOKS[_id];
         }
     }
-    let _previousPage = BOOKS[round.book].book[round.page - 1];
-    byId('gamePreviousTitle').textContent = BOOKS[round.book].title;
+    let _previousPage = ROUND.book.book[ROUND.page - 1];
+    byId('gamePreviousTitle').textContent = ROUND.book.title;
 
     // Change drawing <=> writing mode
-    if (round.type === "Write") {
+    if (ROUND.type === "Write") {
         // Change to drawing mode
-        round.type = "Draw";
+        ROUND.type = "Draw";
         byId('pageType').textContent = "Drawing";
         show(byId('gameDraw'));
         hide(byId('gameWrite'));
@@ -200,9 +195,9 @@ SOCKET.on('nextPage', () => {
         CANVAS.isDrawingMode = true;
         CANVAS.selection = true;
 
-    } else if (round.type === "Draw") {
+    } else if (ROUND.type === "Draw") {
         // Change to writing mode
-        round.type = "Write";
+        ROUND.type = "Write";
         byId('pageType').textContent = "Writing";
         byId('writePrompt').textContent = "What happens next?";
         show(byId('gameWrite'));
@@ -235,7 +230,9 @@ SOCKET.on('startPresenting', () => {
     show(byId('presentSection'));
 
     // Update round state
-    round = {number: undefined, book: undefined, type: "Presenting"}
+    ROUND.page = undefined;
+    ROUND.book = undefined;
+    ROUND.type = "Presenting";
 
     // Add books to DOM for presenting
     updateBookList();
@@ -249,15 +246,15 @@ SOCKET.on('presentBook', (data) => {
     byId('presentSection').classList.remove('presentLobby');
 
     // Keep track of presentation
-    round.book = BOOKS[data.book];
-    round.page = -1;
-    round.presenter = data.presenter;
+    ROUND.book = BOOKS[data.book];
+    ROUND.page = -1;
+    ROUND.presenter = data.presenter;
 
     // Get book title and authors
-    let _title = round.book.title;
-    let _presenter = getName(round.presenter);
+    let _title = ROUND.book.title;
+    let _presenter = getName(ROUND.presenter);
     let _authors = [];
-    round.book.book.forEach((_page) => {
+    ROUND.book.book.forEach((_page) => {
         let _author = getName(_page.author);
         if (_authors.indexOf(_author) === -1) {
             _authors.push(_author);
@@ -270,7 +267,7 @@ SOCKET.on('presentBook', (data) => {
     byId('presentBookPresenter').textContent = "Presented by " + _presenter;
 
     // If you're the presenter, enable controls
-    if (ID === round.presenter) {
+    if (ID === ROUND.presenter) {
         show(byId('presentControls'));
     } else {
         hide(byId('presentControls'));
@@ -280,10 +277,10 @@ SOCKET.on('presentBook', (data) => {
 SOCKET.on('presentForward', (data) => {
     // Go to next page
     byId('inputPresentBack').disabled = false;
-    round.page += 1;
+    ROUND.page += 1;
 
     // Add page to window
-    let _page = round.book.book[round.page];
+    let _page = ROUND.book.book[ROUND.page];
     let _div = document.createElement('div');
     _div.classList.add('page');
     switch (_page.type) {
@@ -309,7 +306,7 @@ SOCKET.on('presentForward', (data) => {
     _window.scrollTop = _window.scrollHeight;
 
     // Last page
-    if (ID === round.presenter && round.page === parseInt(roomSettings.pageCount) - 1) {
+    if (ID === ROUND.presenter && ROUND.page === parseInt(ROOM.settings.pageCount) - 1) {
         byId('inputPresentForward').disabled = true;
         show(byId('presentControlsFinish'));
     }
@@ -318,14 +315,14 @@ SOCKET.on('presentForward', (data) => {
 SOCKET.on('presentBack', (data) => {
     // Go to previous page
     byId('inputPresentForward').disabled = false;
-    round.page -= 1;
+    ROUND.page -= 1;
 
     // Remove last added page
     let _pages = document.querySelectorAll('.page');
     _pages[_pages.length - 1].remove();
 
     // First page
-    if (ID === round.presenter && round.page === -1) {
+    if (ID === ROUND.presenter && ROUND.page === -1) {
         byId('inputPresentBack').disabled = true;
     }
 });
@@ -377,10 +374,14 @@ SOCKET.on('disconnect', (data) => {
 // Restrict inputs with a filter function
 // Get name from ID
 function getName(id) {
-    if (id === ID) {
-        return name;
+    if (id) {
+        if (id === ID) {
+            return name;
+        } else {
+            return USERS[id].name;
+        }
     } else {
-        return USERS[id].name;
+        console.error("ID is undefined");
     }
 }
 
@@ -441,53 +442,52 @@ function copyToClipboard(text) {
 }
 
 
-
 /// --- UPDATE DOM --- ///
 // Update settings
 function updateSettings() {
     // First page
     document.querySelectorAll('input[name=firstPage]').forEach((elem) => {
-        elem.checked = (elem.value === roomSettings.firstPage);
+        elem.checked = (elem.value === ROOM.settings.firstPage);
     });
-    byId('firstPageDisplay').value = roomSettings.firstPage;
+    byId('firstPageDisplay').value = ROOM.settings.firstPage;
 
     // Pages per book
-    byId('pageCount').value = roomSettings.pageCount;
-    byId('pageCountDisplay').value = roomSettings.pageCount;
-    byId('pageMax').textContent = roomSettings.pageCount;
+    byId('pageCount').value = ROOM.settings.pageCount;
+    byId('pageCountDisplay').value = ROOM.settings.pageCount;
+    byId('pageMax').textContent = ROOM.settings.pageCount;
 
     // Page assignment
     document.querySelectorAll('input[name=pageOrder]').forEach((elem) => {
-        elem.checked = (elem.value === roomSettings.pageOrder);
+        elem.checked = (elem.value === ROOM.settings.pageOrder);
     });
-    byId('pageOrderDisplay').value = roomSettings.pageOrder;
+    byId('pageOrderDisplay').value = ROOM.settings.pageOrder;
 
     // Color palette
-    document.querySelector('#colourPalette [value="' + roomSettings.palette + '"]').selected = true;
-    byId('colourPaletteDisplay').value = roomSettings.palette;
+    document.querySelector('#colourPalette [value="' + ROOM.settings.palette + '"]').selected = true;
+    byId('colourPaletteDisplay').value = ROOM.settings.palette;
 
     // Write time limit
-    byId('timeWrite').value = roomSettings.timeWrite;
-    byId('timeWriteDisplay').value = parseInt(roomSettings.timeWrite) ? roomSettings.timeWrite + " min" : 'None';
+    byId('timeWrite').value = ROOM.settings.timeWrite;
+    byId('timeWriteDisplay').value = parseInt(ROOM.settings.timeWrite) ? ROOM.settings.timeWrite + " min" : 'None';
 
     // Draw time limit
-    byId('timeDraw').value = roomSettings.timeDraw;
-    byId('timeDrawDisplay').value = parseInt(roomSettings.timeDraw) ? roomSettings.timeDraw + " min" : 'None';
+    byId('timeDraw').value = ROOM.settings.timeDraw;
+    byId('timeDrawDisplay').value = parseInt(ROOM.settings.timeDraw) ? ROOM.settings.timeDraw + " min" : 'None';
 }
 
 // Update host information on the page
 function updateHost() {
     // Update the host name in the DOM
     document.querySelectorAll('.hostName').forEach((elem) => {
-        elem.textContent = "Host: " + host;
+        elem.textContent = "Host: " + getName(ROOM.host);
     })
 
     // Allow the client to edit settings if they're the host
     document.querySelectorAll('.isHost').forEach((elem) => {
-        isHost ? show(elem) : hide(elem);
+        (ID === ROOM.host) ? show(elem) : hide(elem);
     });
     document.querySelectorAll('.isNotHost').forEach((elem) => {
-        isHost ? hide(elem) : show(elem);
+        (ID === ROOM.host) ? hide(elem) : show(elem);
     });
 }
 
@@ -540,13 +540,13 @@ function updateBookList() {
         _bookAuthor.className = "bookAuthor";
 
         // Display who's working on the current page of the book
-        if (round.type === "Presenting") {
+        if (ROUND.type === "Presenting") {
             _bookTitle.textContent = BOOKS[_id].title;
             _bookAuthor.textContent = "by\u00a0" + BOOKS[_id].author;
         } else {
             let _num, _page, _authorID, _author;
-            _num = (round.page + 1).toString();
-            _page = BOOKS[_id].book[round.page];
+            _num = (ROUND.page + 1).toString();
+            _page = BOOKS[_id].book[ROUND.page];
             _author = getName(_page.author ?? _page);
             _bookTitle.textContent = BOOKS[_id].title;
             _bookAuthor.textContent = "Pg\u00a0" + _num + "\u00a0-\u00a0" + _author;
@@ -659,19 +659,19 @@ function unblur(e) {
 
         if (_inputName.reportValidity() && _inputRoom.reportValidity()) {
             name = _inputName.value.substr(0, 32);
-            room = _inputRoom.value.substr(0, 8);
+            ROOM.code = _inputRoom.value.substr(0, 8);
 
             _inputName.disabled = true;
             _inputRoom.disabled = true;
             e.target.disabled = true;
             show(byId('loading'));
 
-            window.history.pushState({room: room}, '', '?room=' + room);
+            window.history.pushState({roomCode: ROOM.code}, '', '?room=' + ROOM.code);
 
             SOCKET.emit("joinRoom", {
                 id: ID,
                 name: name,
-                room: room,
+                roomCode: ROOM.code,
                 key: SESSION_KEY
             });
         }
@@ -679,15 +679,15 @@ function unblur(e) {
 
     // Setup: Send settings to the server
     function emitSettings() {
-        isHost ? SOCKET.emit("settings", {settings: roomSettings, key: SESSION_KEY}) : SOCKET.disconnect();
+        (ID === ROOM.host) ? SOCKET.emit("settings", {settings: ROOM.settings, key: SESSION_KEY}) : SOCKET.disconnect();
         updateSettings();
     }
 
     // Setup: First page
     document.querySelectorAll('input[name=firstPage]').forEach((elem) => {
         elem.addEventListener('input', (e) => {
-            if (roomSettings) {
-                roomSettings.firstPage = e.target.value;
+            if (ROOM.settings) {
+                ROOM.settings.firstPage = e.target.value;
                 emitSettings();
             }
         })
@@ -699,8 +699,8 @@ function unblur(e) {
         byId('pageCountDisplay').textContent = e.target.value;
     });
     _pageCount.addEventListener('change', (e) => {
-        if (roomSettings) {
-            roomSettings.pageCount = e.target.value;
+        if (ROOM.settings) {
+            ROOM.settings.pageCount = e.target.value;
             emitSettings();
         }
     });
@@ -708,8 +708,8 @@ function unblur(e) {
     // Setup: Page assignment
     document.querySelectorAll('input[name=pageOrder]').forEach((elem) => {
         elem.addEventListener('input', (e) => {
-            if (roomSettings) {
-                roomSettings.pageOrder = e.target.value;
+            if (ROOM.settings) {
+                ROOM.settings.pageOrder = e.target.value;
                 emitSettings();
             }
         });
@@ -717,8 +717,8 @@ function unblur(e) {
 
     // Setup: Colour palette
     byId('colourPalette').addEventListener('input', (e) => {
-        if (roomSettings) {
-            roomSettings.palette = e.target.value;
+        if (ROOM.settings) {
+            ROOM.settings.palette = e.target.value;
             emitSettings();
         }
     });
@@ -729,8 +729,8 @@ function unblur(e) {
         byId('timeWriteDisplay').value = parseInt(e.target.value) ? e.target.value + " min" : "None";
     });
     _timeWrite.addEventListener('change', (e) => {
-        if (roomSettings) {
-            roomSettings.timeWrite = e.target.value;
+        if (ROOM.settings) {
+            ROOM.settings.timeWrite = e.target.value;
             emitSettings();
         }
     });
@@ -741,16 +741,16 @@ function unblur(e) {
         byId('timeDrawDisplay').value = parseInt(e.target.value) ? e.target.value + " min" : "None";
     });
     _timeDraw.addEventListener('change', (e) => {
-        if (roomSettings) {
-            roomSettings.timeDraw = e.target.value;
+        if (ROOM.settings) {
+            ROOM.settings.timeDraw = e.target.value;
             emitSettings();
         }
     });
 
     // Setup: Start game button
     byId('inputStart').addEventListener('click', () => {
-        if (roomSettings) {
-            SOCKET.emit('startGame', {settings: roomSettings, key: SESSION_KEY});
+        if (ROOM.settings) {
+            SOCKET.emit('startGame', {settings: ROOM.settings, key: SESSION_KEY});
         }
     })
 
@@ -765,7 +765,7 @@ function unblur(e) {
     // Setup: Invite copy room
     byId('inputCopyRoom').addEventListener('click', () => {
         // Add room code to clipboard
-        copyToClipboard(room);
+        copyToClipboard(ROOM.code);
         byId('copyDisplay').innerText = "Room code copied!";
     });
 
@@ -787,12 +787,12 @@ function unblur(e) {
     // Game: Submit page
     byId('inputPageSubmit').addEventListener('click', (e) => {
         // Get write input if it's a writing round
-        if (round.type === "Write") {
+        if (ROUND.type === "Write") {
             let _inputWrite = byId('inputWrite');
             if (_inputWrite.reportValidity()) {
                 // Send page to the server
                 let _value = _inputWrite.value.substr(0, 80);
-                SOCKET.emit('submitPage', {type: round.type, value: _value, key: SESSION_KEY});
+                SOCKET.emit('submitPage', {type: ROUND.type, value: _value, key: SESSION_KEY});
 
                 // Put client in waiting state
                 e.target.disabled = true;
@@ -803,7 +803,7 @@ function unblur(e) {
         }
 
         // Get draw input if it's a drawing round
-        else if (round.type === "Draw") {
+        else if (ROUND.type === "Draw") {
             // Export canvas data to base64
             let _value = CANVAS.toDataURL({
                 format: 'png',
@@ -811,7 +811,7 @@ function unblur(e) {
             });
 
             // Send page to the server
-            SOCKET.emit('submitPage', {type: round.type, value: _value, key: SESSION_KEY});
+            SOCKET.emit('submitPage', {type: ROUND.type, value: _value, key: SESSION_KEY});
 
             // Put client in waiting state
             e.target.disabled = true;
@@ -830,8 +830,8 @@ function unblur(e) {
 
     // Present: Next page
     byId('inputPresentForward').addEventListener('click', () => {
-        if (ID === round.presenter) {
-            if (round.page < parseInt(roomSettings.pageCount) - 1) {
+        if (ID === ROUND.presenter) {
+            if (ROUND.page < parseInt(ROOM.settings.pageCount) - 1) {
                 SOCKET.emit('presentForward', {key: SESSION_KEY});
             }
         }
@@ -839,8 +839,8 @@ function unblur(e) {
 
     // Present: Previous page
     byId('inputPresentBack').addEventListener('click', () => {
-        if (ID === round.presenter) {
-            if (round.page > -1) {
+        if (ID === ROUND.presenter) {
+            if (ROUND.page > -1) {
                 SOCKET.emit('presentBack', {key: SESSION_KEY});
             }
         }
@@ -848,7 +848,7 @@ function unblur(e) {
 
     // Present: Finished
     byId('inputPresentFinish').addEventListener('click', () => {
-        if (ID === round.presenter) {
+        if (ID === ROUND.presenter) {
             SOCKET.emit('presentFinish', {key: SESSION_KEY});
         }
     });
