3908820fa9c8188b77a866e79321b2a9146c5e5c
Bunny <12066089+glitchybunny@users.noreply.github.com>
2021-09-24 11:06:56 +1000

    Moved ShaderPath class to fabric-regl.js

--- static/js/fabric-regl.js
+++ static/js/fabric-regl.js
@@ -34,14 +34,6 @@ void main() {
   gl_FragColor = color;
 }`;
 
-const createLineMesh = (points) => {
-	let buffer = [];
-	for (let i = 0; i < (points - 1) * 2; i += 2) {
-		buffer.push(i, i + 1, i + 2, i + 2, i + 1, i + 3);
-	}
-	return buffer;
-};
-
 const buffer = {
 	duplicate(buffer, stride) {
 		const out = [];
@@ -57,6 +49,39 @@ const buffer = {
 		return out;
 	},
 };
+
+function createPositions(path) {
+	let prev, positions;
+	positions = [];
+
+	// iterate over path and generate array of positions
+	for (let i of path) {
+		switch (i[0]) {
+			case "M": // moveTo
+			case "L": // lineto
+				positions.push(i[1], i[2], i[1], i[2]); // push first and last twice
+				prev = [i[1], i[2]];
+				break;
+			case "Q": // quadraticCurveTo
+				// generate approximate midpoints along the quadratic bezier (for smoother lines)
+				positions.push(
+					prev[0] * 0.5625 + i[1] * 0.375 + i[3] * 0.0625,
+					prev[1] * 0.5625 + i[2] * 0.375 + i[4] * 0.0625,
+					prev[0] * 0.25 + i[1] * 0.5 + i[3] * 0.25,
+					prev[1] * 0.25 + i[2] * 0.5 + i[4] * 0.25,
+					prev[0] * 0.0625 + i[1] * 0.375 + i[3] * 0.5625,
+					prev[1] * 0.0625 + i[2] * 0.375 + i[4] * 0.5625
+				);
+
+				// add positions
+				positions.push(i[3], i[4]);
+				prev = [i[3], i[4]];
+				break;
+		}
+	}
+
+	return positions;
+}
 
 const createPositionBuffer = (positions, points) => {
 	let positionDup = new Float32Array(buffer.duplicate(positions, 2));
@@ -78,106 +103,95 @@ const createOffsetBuffer = (points) => {
 	});
 };
 
-/*
-// make canvas
-const canvas = document.createElement("canvas");
-canvas.width = 800;
-canvas.height = 600;
-document.body.appendChild(canvas);
-
-const regl = wrapREGL({
-	canvas: canvas,
-	pixelRatio: 1,
-	attributes: { antialias: false, preserveDrawingBuffer: true },
-});
-
-// compute line values
-const positions = [
-	137.24604966139955, 121.97990435513897, 138.14898419864562, 121.98490435513897,
-	139.05191873589166, 122.88715948340288, 139.9548532731377, 135.5187312790978, 142.66365688487585,
-	156.27059922916806, 145.372460496614, 174.3157017944465, 147.17832957110608, 195.06756974451673,
-	151.69300225733633, 219.42845820764268, 156.2076749435666, 241.0825812859768, 158.01354401805867,
-	259.12768385125526, 160.72234762979684, 273.56376590347804, 164.33408577878106, 282.5863171861173,
-	167.94582392776522, 289.80435821222864, 174.26636568848758, 297.0223992383401, 180.58690744920995,
-	302.4359300079236, 183.29571106094807, 305.1426953927154, 185.1015801354402, 306.94720564924324,
-	187.81038374717832, 307.84946077750715, 190.5191873589165, 306.94720564924324, 194.13092550790068,
-	304.2404402644515, 198.6455981941309, 298.8269094948679, 202.25733634311513, 290.7066133404926,
-	204.06320541760724, 278.9772966730616, 204.96613995485328, 266.3457248773667, 206.77200902934538,
-	253.71415308167178, 209.4808126410835, 242.8870915425047, 211.2866817155756, 234.76679538812937,
-	212.18961625282168, 229.35326461854584, 213.99548532731376, 228.4510094902819, 216.70428893905193,
-	231.1577748750737, 220.31602708803612, 238.3758159011851, 227.53950338600453, 249.20287744035215,
-	234.7629796839729, 262.736704364311, 241.9864559819413, 278.07504154479767, 249.2099322799097,
-	288.9021030839648, 252.82167042889392, 294.3156338535483, 256.43340857787814, 299.72916462313185,
-	261.8510158013544, 306.94720564924324, 267.26862302483073, 313.2629915470907, 269.97742663656885,
-	315.96975693188244, 271.78329571106093, 316.8720120601464, 275.39503386004515, 315.06750180361854,
-	279.00677200902936, 310.5562261622989, 282.6185101580136, 303.3381851361875, 287.13318284424383,
-	287.09759282743687, 289.84198645598195, 265.44346974910275, 293.45372460496617, 243.7893466707686,
-	297.9683972911964, 223.03747872069835, 299.7742663656885, 204.9923761554199, 299.7742663656885,
-	186.94727359014144, 299.7742663656885, 171.60893640965475, 299.7742663656885, 164.39089538354335,
-	299.7742663656885, 160.78187487048768, 299.7742663656885, 158.0701094856959,
-];
-const POINTS = positions.length / 2;
-buffer.pushElement(positions, POINTS - 1, 2);
-buffer.unshiftElement(positions, 0, 2);
-
-const positionsDup = new Float32Array(buffer.duplicate(positions, 2));
-const positionBuffer = regl.buffer({
-	usage: "static",
-	type: "float",
-	length: (POINTS + 2) * 4 * FLOAT_BYTES,
-	data: positionsDup,
-});
-
-const offset = new Array(POINTS * 2).fill().map((v, i) => 1 - (i % 2) * 2); // alternating [1, -1, 1, -1, etc]
-const offsetBuffer = regl.buffer({
-	usage: "static",
-	type: "float",
-	length: (POINTS + 2) * 2 * FLOAT_BYTES,
-	data: offset,
-});
-
-const mesh = lineMesh(POINTS);
-
-// regl inputs
-const attributes = {
-	prevPos: {
-		buffer: positionBuffer,
-		offset: 0,
-		stride: FLOAT_BYTES * 2,
-	},
-	currPos: {
-		buffer: positionBuffer,
-		offset: FLOAT_BYTES * 2 * 2,
-		stride: FLOAT_BYTES * 2,
-	},
-	nextPos: {
-		buffer: positionBuffer,
-		offset: FLOAT_BYTES * 2 * 4,
-		stride: FLOAT_BYTES * 2,
-	},
-	offset: offsetBuffer,
+const createLineMesh = (points) => {
+	let buffer = [];
+	for (let i = 0; i < (points - 1) * 2; i += 2) {
+		buffer.push(i, i + 1, i + 2, i + 2, i + 1, i + 3);
+	}
+	return buffer;
 };
 
-const uniforms = {
-	color: [0.5, 0, 0.5, 1],
-	thickness: 2.5,
-};
+// ShaderPath Class to render crisp lines using WebGL
+class ShaderPath extends fabric.Path {
+	constructor(path) {
+		super();
 
-const elements = regl.elements({
-	primitive: "triangles",
-	usage: "static",
-	type: "uint16",
-	data: mesh,
-});
+		// copy attributes from path
+		for (let _ of Object.keys(path)) {
+			this[_] = path[_];
+		}
 
-// render single frame
-window.requestAnimationFrame(
-	regl({
-		attributes,
-		uniforms,
-		elements,
-		vert,
-		frag,
-	})
-);
-*/
+		// precalculate positions, buffers, mesh, etc for rendering
+		this._precalculatePath();
+	}
+
+	_precalculatePath() {
+		let _positions, _points, _positionBuffer, _offsetBuffer, _lineMesh;
+		_positions = createPositions(this.path);
+		_points = _positions.length / 2 - 2;
+		_positionBuffer = createPositionBuffer(_positions, _points);
+		_offsetBuffer = createOffsetBuffer(_points);
+		_lineMesh = createLineMesh(_points);
+
+		this.attributes = {
+			prevPos: {
+				buffer: _positionBuffer,
+				offset: 0,
+				stride: FLOAT_BYTES * 2,
+			},
+			currPos: {
+				buffer: _positionBuffer,
+				offset: FLOAT_BYTES * 2 * 2,
+				stride: FLOAT_BYTES * 2,
+			},
+			nextPos: {
+				buffer: _positionBuffer,
+				offset: FLOAT_BYTES * 2 * 4,
+				stride: FLOAT_BYTES * 2,
+			},
+			offset: _offsetBuffer,
+		};
+
+		this.elements = regl.elements({
+			primitive: "triangles",
+			usage: "static",
+			type: "uint16",
+			data: _lineMesh,
+		});
+	}
+
+	_renderPathCommands(ctx) {
+		// drawing parameters
+		let color, thickness, w, h;
+		color = fabric.Color.fromHex(this.stroke)._source;
+		thickness = this.strokeWidth / 2;
+		w = CANVAS_3.width;
+		h = CANVAS_3.height;
+
+		// draw the line
+		let render = regl({
+			attributes: this.attributes,
+			uniforms: {
+				color: [color[0] / 255, color[1] / 255, color[2] / 255, 1],
+				thickness: thickness,
+				width: w,
+				height: h,
+				scale: w / 800,
+			},
+			elements: this.elements,
+			vert: SHD_LINE_VERT,
+			frag: SHD_LINE_FRAG,
+			viewport: {
+				x: 0,
+				y: 0,
+				width: w,
+				height: h,
+			},
+		});
+		render();
+
+		// clean up render object nto free memory
+		render.destroy();
+		render = null;
+	}
+}
